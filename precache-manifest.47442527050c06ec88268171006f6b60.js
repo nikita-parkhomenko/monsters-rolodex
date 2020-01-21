42410086bffdcdd95b78872a1fb293cf025dbb34
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-01-21 18:32:30 +0200

    Updates

---
+++ precache-manifest.47442527050c06ec88268171006f6b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804a2b81c0b1fa7dc5674183e6a4e9a7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "37b9f380261b95a899e1",
+    "url": "/monsters-rolodex/static/css/main.645c7e05.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37b9f380261b95a899e1",
+    "url": "/monsters-rolodex/static/js/main.92f6a0af.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
